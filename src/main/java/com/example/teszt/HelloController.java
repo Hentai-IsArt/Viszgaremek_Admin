4679444aaf1adb9fd66fa6859f70fde282a32f51
Hentai-IsArt <Ihatemylifefrfr69@gmail.com>
2024-10-15 11:27:52 +0200

    Scene létrehozzása

--- src/main/java/com/example/teszt/HelloController.java
+++ src/main/java/com/example/teszt/HelloController.java
@@ -1,14 +1,19 @@
 package com.example.teszt;
 
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.scene.control.Button;
+import javafx.scene.control.MenuBar;
+import javafx.scene.control.TextField;
 
 public class HelloController {
-    @FXML
-    private Label welcomeText;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
-    }
+    private Button button;
+
+    @FXML
+    private MenuBar menu;
+
+    @FXML
+    private TextField search;
+
 }
